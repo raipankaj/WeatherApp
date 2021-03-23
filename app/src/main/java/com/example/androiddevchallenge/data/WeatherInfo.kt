958fab8169f3a0a37c1089b7e2f9e5f4c113667e
Pankaj <pankaj.rai16@gmail.com>
2021-03-23 19:03:10 +0530

    Updated repo class

--- app/src/main/java/com/example/androiddevchallenge/data/WeatherInfo.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/WeatherInfo.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.data
 
 /**
@@ -5,6 +20,7 @@ package com.example.androiddevchallenge.data
  * weather.
  */
 data class WeatherInfo(
-    val temperature: String, val date: String,
+    val temperature: String,
+    val date: String,
     val weatherImage: Int
 )
